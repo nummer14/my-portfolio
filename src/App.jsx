db407fe8b127a65e608ae9a5fbc5389bf1c6c561
nummer14 <nummer14@naver.com>
2025-08-12 15:50:24 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import AboutSection from "./components/AboutSection";
+import ExperienceSection from "./components/ExperienceSection";
+import Header from "./components/Header";
+import ProjectsSection from "./components/ProjectsSection";
+import SkillsSection from "./components/SkillsSection";
+import WritingSection from "./components/WritingSection";
+import portfolioData from "./portfolio-data.json";
+
+const App = () => {
+  return (
+    <div className="min-h-screen">
+      <Header profile={portfolioData.profile} />
+      <AboutSection profile={portfolioData.profile} />
+      <SkillsSection skills={portfolioData.skills} />
+      <ProjectsSection projects={portfolioData.projects} />
+      <ExperienceSection
+        experience={portfolioData.experience}
+        education={portfolioData.education}
+        certifications={portfolioData.certifications}
+      />
+      <WritingSection
+        writing={portfolioData.writing}
+        talks={portfolioData.talks}
+        openSource={portfolioData.open_source}
+      />
+      <footer className="py-8 px-4 text-center">
+        <p className="text-sm">
+          © 2025 {portfolioData.profile.name}. All right reserved.
+        </p>
+      </footer>
+    </div>
+  );
+};
+
+export default App;
